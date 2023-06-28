19d97c6cb1ce9d71f5bbd0430a7043a2de08d296
EstelleWho <122816900+EstelleWho@users.noreply.github.com>
2023-06-28 03:59:24 -0400

    add submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -28,17 +28,75 @@ const data = [
     "created_at": 1461113959088
   }
 ]
+// TWEET
+$(document).ready(function() {
+  const createTweetElement = function(tweet) {
+  let $tweet = $(`
+  <article class="tweet">
+    <header>
+      <div class="user-info">
+        <img class="avatar" src="${tweet.user.avatars}" alt="User Avatar">
+        <span class="user-name">${tweet.user.name}</span>
+      </div>
+      <span class="user-handle">${tweet.user.handle}</span>
+    </header>
+    <div class="tweet-content">
+      ${tweet.content.text}
+    </div>
+    <footer>
+      <span class="tweet-timestamp">${timeago.format(tweet.created_at)}</span>
+      <div class="actions">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+  </article>
+    `);
+  return $tweet;
+  };
 
+// RENDERING TWEETS
 const renderTweets = function(tweets) {
 // loops through tweets
 // calls createTweetElement for each tweet
 // takes return value and appends it to the tweets container
-}
+const $tweetsContainer = $('#tweets-container'); // this is jQuery
+$tweetsContainer.empty(); 
+// the loop
+for (const tweet of tweets) {
+  const $tweet = createTweetElement(tweet);
+  $tweetsContainer.append($tweet);
+  };
+};
 
-const createTweetElement = function(tweet) {
-let $tweet = /* Your code for creating the tweet element */
-// ...
-return $tweet;
-}
+// SUBMISSION
+$('form').on('submit', function(event) {
+  event.preventDefault(); //handler function to prevent default from submission
+  const data = $(this).serialize();
+  console.log("data:", data);
 
-renderTweets(data);
+  $.ajax({
+    type: 'POST',
+    url: "http://localhost:8080/tweets",
+    data,
+    success: function(res) {
+      console.log('Success');
+      console.log(res);
+    },
+    error: function(error) {
+      console.log('Failed')
+      console.log(error);
+      },
+    });
+  $('#tweet-text').val('');
+  $('.counter').val('140');
+  });
+
+// LOAD TWEETS
+
+
+
+
+
+});
